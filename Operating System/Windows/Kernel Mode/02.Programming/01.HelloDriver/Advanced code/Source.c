9d02207566a9c77b93e4c0d006de2c4bb2714dc5
nila27 <68037709+nila27@users.noreply.github.com>
2020-07-11 02:11:58 +0900

    Update Source.c

--- Operating System/Windows/Kernel Mode/02.Programming/01.HelloDriver/Advanced code/Source.c
+++ Operating System/Windows/Kernel Mode/02.Programming/01.HelloDriver/Advanced code/Source.c
@@ -2,17 +2,19 @@
 
 VOID DriverUnload(_In_ PDRIVER_OBJECT DriverObject)
 {
-	_Unreferenced_parameter_(DriverObject);
+	UNREFERENCED_PARAMETER(DriverObject);
 }
 
 
-NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject,_In_ PUNICODE_STRING RegistryPath)
+extern "C" NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject,_In_ PUNICODE_STRING RegistryPath)
 {
 	UNREFERENCED_PARAMETER(DriverObject);
 	UNREFERENCED_PARAMETER(RegistryPath);
 
 
 	DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "Hello Driver");
+
+	DriverObject->DriverUnload = DriverUnload;
 
 
 	return STATUS_SUCCESS;
