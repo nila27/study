ce5b40df2994c70f866624f568d15989cd6fe1cd
nila27 <68037709+nila27@users.noreply.github.com>
2020-07-11 03:57:59 +0900

    Update Source.cpp

--- Operating System/Windows/Kernel Mode/02.Programming/01.HelloDriver/Advanced code/Source.cpp
+++ Operating System/Windows/Kernel Mode/02.Programming/01.HelloDriver/Advanced code/Source.cpp
@@ -8,7 +8,6 @@ VOID DriverUnload(_In_ PDRIVER_OBJECT DriverObject)
 
 extern "C" NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject,_In_ PUNICODE_STRING RegistryPath)
 {
-	UNREFERENCED_PARAMETER(DriverObject);
 	UNREFERENCED_PARAMETER(RegistryPath);
 
 
