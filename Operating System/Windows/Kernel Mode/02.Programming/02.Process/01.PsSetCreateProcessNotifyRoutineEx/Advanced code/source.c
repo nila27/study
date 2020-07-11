bdbe81ad94d510751336e8aadb0d0c977b6f5a09
nila27 <68037709+nila27@users.noreply.github.com>
2020-07-12 04:27:49 +0900

    Create source.c

---
+++ Operating System/Windows/Kernel Mode/02.Programming/02.Process/01.PsSetCreateProcessNotifyRoutineEx/Advanced code/source.c
@@ -0,0 +1,43 @@
+#include <ntddk.h>
+
+VOID CreateProcessNotifyRoutineEx(PEPROCESS Process, HANDLE ProcessId, PPS_CREATE_NOTIFY_INFO CreateInfo)
+{
+    UNREFERENCED_PARAMETER(Process);
+    UNREFERENCED_PARAMETER(ProcessId);
+
+
+    PLIST_ENTRY pNode = (PLIST_ENTRY)((unsigned char*)Process);
+
+    if (CreateInfo == NULL)
+    {
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "Process Exit");
+    }
+    else
+    {
+        
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "Processs Name   :   (%s)\n", (const char*)(unsigned char*)pNode + 0x450);
+
+    }
+}
+
+VOID DriverUnload(_In_ PDRIVER_OBJECT driverobject)
+{
+	UNREFERENCED_PARAMETER(driverobject);
+    PsSetCreateProcessNotifyRoutineEx(CreateProcessNotifyRoutineEx, TRUE);
+
+    DbgPrintEx(DPFLTR_IHVAUDIO_ID, 0, "Stop");
+}
+
+NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT driverobject, _In_ PUNICODE_STRING RegistryPath)
+{
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+    driverobject->DriverUnload = DriverUnload;
+
+    DbgPrintEx(DPFLTR_IHVAUDIO_ID, 0, "Start");
+
+
+    PsSetCreateProcessNotifyRoutineEx(CreateProcessNotifyRoutineEx, FALSE);
+
+    return STATUS_SUCCESS;
+}
