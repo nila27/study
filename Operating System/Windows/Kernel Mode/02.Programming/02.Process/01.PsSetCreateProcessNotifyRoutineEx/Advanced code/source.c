674482cc8a93923e0eca2c0f4adac76b3ddabc60
nila27 <68037709+nila27@users.noreply.github.com>
2020-07-12 05:18:07 +0900

    Update source.c

--- Operating System/Windows/Kernel Mode/02.Programming/02.Process/01.PsSetCreateProcessNotifyRoutineEx/Advanced code/source.c
+++ Operating System/Windows/Kernel Mode/02.Programming/02.Process/01.PsSetCreateProcessNotifyRoutineEx/Advanced code/source.c
@@ -5,8 +5,7 @@ VOID CreateProcessNotifyRoutineEx(PEPROCESS Process, HANDLE ProcessId, PPS_CREAT
     UNREFERENCED_PARAMETER(Process);
     UNREFERENCED_PARAMETER(ProcessId);
 
-
-    PLIST_ENTRY pNode = (PLIST_ENTRY)((unsigned char*)Process);
+    PEPROCESS PP = Process;
 
     if (CreateInfo == NULL)
     {
@@ -15,7 +14,7 @@ VOID CreateProcessNotifyRoutineEx(PEPROCESS Process, HANDLE ProcessId, PPS_CREAT
     else
     {
         
-        DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "Processs Name   :   (%s)\n", (const char*)(unsigned char*)pNode + 0x450);
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "Processs Name   :   (%s)\n", (const char*)(unsigned char*)PP + 0x450);
 
     }
 }
