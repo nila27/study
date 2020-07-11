80e8d175bbc36e5afc41b5d69c5f279d4a3a461c
nila27 <68037709+nila27@users.noreply.github.com>
2020-07-12 04:26:16 +0900

    Update Source.c

--- Operating System/Windows/Kernel Mode/02.Programming/02.Process/01.PsSetCreateProcessNotifyRoutineEx/Source.c
+++ Operating System/Windows/Kernel Mode/02.Programming/02.Process/01.PsSetCreateProcessNotifyRoutineEx/Source.c
@@ -5,23 +5,22 @@ VOID CreateProcessNotifyRoutineEx(PEPROCESS Process, HANDLE ProcessId, PPS_CREAT
     UNREFERENCED_PARAMETER(Process);
     UNREFERENCED_PARAMETER(ProcessId);
 
-
-    PLIST_ENTRY pNode = (PLIST_ENTRY)((unsigned char*)Process + 0x450);
-
     if (CreateInfo == NULL)
     {
         DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "Process Exit");
     }
     else
     {
-        DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "Process Name   :   (%s)\n", (const char*)(unsigned char*)pNode);
-
+        
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "Processs Name   :   (%wZ)\n", CreateInfo->ImageFileName);
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "Processs Name   :   (%wZ)\n", CreateInfo->CommandLine);
+	    
     }
 }
 
 VOID DriverUnload(_In_ PDRIVER_OBJECT driverobject)
 {
-	UNREFERENCED_PARAMETER(driverobject);
+    UNREFERENCED_PARAMETER(driverobject);
     PsSetCreateProcessNotifyRoutineEx(CreateProcessNotifyRoutineEx, TRUE);
 
     DbgPrintEx(DPFLTR_IHVAUDIO_ID, 0, "Stop");
